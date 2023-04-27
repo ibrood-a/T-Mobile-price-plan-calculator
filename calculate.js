16b841309854b3ab279809fc49eb5aedcba078e6
Jacob Kennedy <iblooze2@gmail.com>
2023-04-27 12:12:12 -0500

    Update calculate.js
    removed debugging stuff, renamed some functions, and added support for new plans

--- calculate.js
+++ calculate.js
@@ -8,7 +8,7 @@ const magentaLineCost = [[75, 55, 25], [55, 25, 40], [60, 30, 15]];
 const essentialsLineCost = [[65, 35, 20], [45, 20], [0, 0, 0]];
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-function correctMistakes() {
+function displayPrice() {
     var lineCount = document.getElementById('lineCount').value;
     var freeLine = document.getElementById('freeLine').checked;
     var discountIndex = document.getElementById('discountIndex').selectedIndex;
@@ -16,14 +16,14 @@ function correctMistakes() {
 
     document.getElementById('monthlyPrice').innerHTML = "Monthly Cost: " + parseFloat(cost).toFixed(2);
 
-    if (lineCount > 6 && ratePlanIndex == 2)
+    if (lineCount > 6 && ratePlanIndex == 4)
         document.getElementById('monthlyPrice').innerHTML = "Essentials is limited to six lines";
 
     switch (discountIndex) {
         case senior:
             if (freeLine) 
                 document.getElementById('monthlyPrice').innerHTML = "55+ is not Eligible for Free Line"
-            if (lineCount > 2 && ratePlanIndex === 2)
+            if (lineCount > 2 && ratePlanIndex === 4)
                 document.getElementById('monthlyPrice').innerHTML = "Essentials 55+ is limited to 2 lines";
             if (lineCount > 4)
                 document.getElementById('monthlyPrice').innerHTML = "55+ Plans are limited to 4 lines";
@@ -31,12 +31,12 @@ function correctMistakes() {
         case military:
             if (freeLine) 
                 document.getElementById('monthlyPrice').innerHTML = "Military is not Eligible for Free Line"
-            if (ratePlanIndex === 2)
+            if (ratePlanIndex === 4)
                 document.getElementById('monthlyPrice').innerHTML = "Military plan cannot be used with Essentials";
             break;
         case insider:
             if (ratePlanIndex != 0)
-                document.getElementById('monthlyPrice').innerHTML = "Insider code can only be used with Magenta Max";
+                document.getElementById('monthlyPrice').innerHTML = "Insider code can only be used with Go5G Plus";
             break;
     }
 }
@@ -51,52 +51,48 @@ function calculatePrice() {
     var discountIndex = document.getElementById('discountIndex').selectedIndex;
     var discountTypeIndex = (discountIndex === senior) ? 1 : (discountIndex == military) ? 2 : 0;
     for (var line = 0; line < lineCount; line++) {
-        console.log(line);
-        console.log(freeLine);
-        console.log(discountTypeIndex);
-        if (freeLine && line === 2) {
-            console.log("broken");
-            continue;
-        }
-
         switch (ratePlanIndex) {    
-            case 4:
+            case 4: // Essentials
                 cost += (essentialsLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
                 break;
 
-            case 3:
+            case 3: // Magenta
                 cost += (magentaLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
                 break;
 
-            case 2:
+            case 2: // Magenta Max
                 cost += (maxLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
                 break;
                 
-           case 1:
+           case 1: // Go5G
                 cost += (magentaLineCost[+discountTypeIndex][clamp(line, 0, 2)] + 5);
                 break;
 
-            case 0:
+            case 0: // Go5G Plus
                 cost += (maxLineCost[+discountTypeIndex][clamp(line, 0, 2)] + 5);
                 break;
         }
 
-        // up to 8 lines with autopay
+        // up to 8 lines with $5 autopay discount
         if (line <= 7 && autoPay)
-            cost -= 5; // take $5 minus discount from each line
+            cost -= 5; 
     }
  
+    // insider code is 20% off and can only be used on max
     if (discountIndex === insider && ratePlanIndex === 0)
         cost *= 0.80;
 
+    // add the cost before that way taxes get calculated if needed. 
+     cost += Number(document.getElementById('additionalCostInput').value);
+    
+    // essentials is the only plan not tax inclusive 
+    if (ratePlanIndex == 4)
+        cost *= 1.15;
 
-    if (ratePlanIndex == 2)
-        cost *= 1.12;
-
+    // do this after because taxes paid up front. 
     cost += Number(document.getElementById('deviceCostInput').value);
-    cost += Number(document.getElementById('additionalCostInput').value);
 
-    correctMistakes();
+    displayPrice();
 }
 
 function hideElement(label) {
@@ -131,24 +127,23 @@ function updateP360() {
 
 
 function calculateDownPayment() {
-    var DCCNumber = document.getElementById('DCCNumber').value;
-    var deviceCostInput = document.getElementById('deviceCostInput2').value;
-    var downPaymentInput = document.getElementById('downPaymentInput').value;
-    var financingLimitInput = document.getElementById('financingLimitInput').value;
-    var currentEIPBalanceInput = document.getElementById('currentEIPBalanceInput').value;
-    var accessoryDownCheck = document.getElementById('halfDownCheck').checked;
-    var accessoryFinanceCheck = document.getElementById('accessoryFinanceCheck').checked;
-    var accessoryCostInput = document.getElementById('accessoryCostInput').value;
-    console.log(deviceCostInput);
-    console.log(downPaymentInput);
-    console.log(deviceCostInput - downPaymentInput);
-    console.log(financingLimitInput - currentEIPBalanceInput);
-
+    
     var cost = 0;
     var downPayment = 0;
     var activationCost = 0;
     var accessoryDownPayment = 0;
 
+    var DCCNumber = document.getElementById('DCCNumber').value;
+    var deviceCostInput = document.getElementById('deviceCostInput2').value;
+    var downPaymentInput = document.getElementById('downPaymentInput').value;
+    var accessoryCostInput = document.getElementById('accessoryCostInput').value;
+    var financingLimitInput = document.getElementById('financingLimitInput').value;
+    var currentEIPBalanceInput = document.getElementById('currentEIPBalanceInput').value;
+    
+    var accessoryDownCheck = document.getElementById('halfDownCheck').checked;
+    var accessoryFinanceCheck = document.getElementById('accessoryFinanceCheck').checked;
+    
+    // if they wanna finance more than limit then the downpayment is greater of available or standard downpayment.
     if ((deviceCostInput - downPaymentInput) >= (financingLimitInput - currentEIPBalanceInput)) {
         downPayment = deviceCostInput - (financingLimitInput - currentEIPBalanceInput);
     }
@@ -156,24 +151,21 @@ function calculateDownPayment() {
         downPayment = downPaymentInput;
     }
 
+    // number of activations charged * $35
     activationCost = DCCNumber * 35;
-    if (accessoryDownCheck && accessoryFinanceCheck)
-        accessoryDownPayment = accessoryCostInput * 0.5;
-    else if (!accessoryFinanceCheck)
+    
+    // financing accessories?
+    if (accessoryFinanceCheck) {
+        // 50% downpayment on the accessories. 
+        if (accessoryDownCheck)
+            accessoryDownPayment = accessoryCostInput * 0.5;
+    }
+    else // not financing so full cost. 
         accessoryDownPayment = accessoryCostInput;
 
     var taxes = (parseFloat(deviceCostInput) + parseFloat(accessoryCostInput) + parseFloat(activationCost)) * (taxRate - 1);
     cost = parseFloat(downPayment) + parseFloat(taxes) + parseFloat(accessoryDownPayment) + parseFloat(activationCost);
 
-
-    console.log(deviceCostInput);
-    console.log(accessoryCostInput);
-    console.log(activationCost);
-    console.log(downPayment);
-    console.log(taxes);
-    console.log(accessoryDownPayment);
-    console.log(activationCost);
-    console.log(cost);
-
+    // update the price on user end. 
     document.getElementById('todayPrice').innerHTML = "Today's Cost: $" + parseFloat(cost).toFixed(2);
 }
