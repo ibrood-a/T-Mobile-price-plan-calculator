f31774c36ebdcb278afa057e709f746f65e349e6
Jacob Kennedy <iblooze2@gmail.com>
2023-09-20 15:37:28 -0500

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -3,6 +3,11 @@ const senior = 1;
 const military = 2;
 const insider = 3;
 const taxRate = 1.0825;
+
+const go5GLineCost = [[80, 60, 30], [60, 30, 45], [65, 35, 15]];
+const go5GPlusLineCost = [[95, 65, 40], [75, 35, 55], [80, 40, 30]];
+const go5GNextLineCost = [[105, 75, 50], [85, 45, 65], [90, 50, 40]];
+
 const maxLineCost = [[90, 60, 35], [70, 30, 50], [75, 35, 25]];
 const magentaLineCost = [[75, 55, 25], [55, 25, 40], [60, 30, 15]];
 const essentialsLineCost = [[65, 35, 20], [45, 20], [0, 0, 0]];
@@ -17,8 +22,9 @@ function displayPrice() {
 
     document.getElementById('monthlyPrice').innerHTML = "Monthly Cost: $" + parseFloat(cost).toFixed(2);
 
-    if (lineCount > 6 && ratePlanIndex == 4)
+    if (lineCount > 6 && ratePlanIndex == 5)
         document.getElementById('monthlyPrice').innerHTML = "Essentials is limited to six lines";
+
 
     switch (discountIndex) {
         case senior:
@@ -36,10 +42,13 @@ function displayPrice() {
                 document.getElementById('monthlyPrice').innerHTML = "Military plan cannot be used with Essentials";
             break;
         case insider:
-            if (ratePlanIndex != 0)
+            if (ratePlanIndex > 1)
                 document.getElementById('monthlyPrice').innerHTML = "Insider code can only be used with Go5G Plus";
             break;
     }
+
+    if (lineCount > 12)
+        document.getElementById('monthlyPrice').innerHTML = "We can only do 12 lines in the store.";
 }
 
 function calculatePrice() {
@@ -51,47 +60,65 @@ function calculatePrice() {
     var ratePlanIndex = document.getElementById('ratePlanIndex').selectedIndex;
     var discountIndex = document.getElementById('discountIndex').selectedIndex;
     var discountTypeIndex = (discountIndex === senior) ? 1 : (discountIndex == military) ? 2 : 0;
-    for (var line = 0; line < lineCount; line++) {
-        if (line == 2 && freeLine) 
-            continue; 
+
+
+     for (var line = 0; line < lineCount; line++) {
+
+        if (line == 2 && freeLine) {
+            if (ratePlanIndex === 0) // go5G Next
+                cost += (go5GNextLineCost[+discountTypeIndex][clamp(line, 0, 2)] - 35);
+            else
+                continue;
+        } else {
+            switch (ratePlanIndex) {
+                case 7: // go5G Plus
+                    cost += (go5GPlusLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 6: // go5G
+                    cost += (go5GLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 5: // Essentials
+                    cost += (essentialsLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 4: // Magenta
+                    cost += (magentaLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 3: // Magenta Max
+                    cost += (maxLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 2: // Go5G
+                cost += (go5GLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 1: // Go5G Plus
+                cost += (go5GPlusLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+                case 0: // go5G Next
+                    cost += (go5GNextLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
+                    break;
+            }
+        }
         
-        switch (ratePlanIndex) {    
-            case 4: // Essentials
-                cost += (essentialsLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
-                break;
-
-            case 3: // Magenta
-                cost += (magentaLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
-                break;
-
-            case 2: // Magenta Max
-                cost += (maxLineCost[+discountTypeIndex][clamp(line, 0, 2)]);
-                break;
-                
-           case 1: // Go5G
-                cost += (magentaLineCost[+discountTypeIndex][clamp(line, 0, 2)] + 5);
-                break;
-
-            case 0: // Go5G Plus
-                cost += (maxLineCost[+discountTypeIndex][clamp(line, 0, 2)] + 5);
-                break;
+        // up to 8 lines with $5 autopay discount
+        if (line <= 7 && autoPay) {
+            cost -= 5;
         }
 
-        // up to 8 lines with $5 autopay discount
-        if (line <= 7 && autoPay)
-            cost -= 5; 
+        if (line > 7)
+            cost += 5;
     }
- 
+
     // insider code is 20% off and can only be used on max
-    if (discountIndex === insider && ratePlanIndex === 0)
+    if (discountIndex === insider && ratePlanIndex <= 1) {
         cost *= 0.80;
+    }
 
     // add the cost before that way taxes get calculated if needed. 
-     cost += Number(document.getElementById('additionalCostInput').value);
-    
-    // essentials is the only plan not tax inclusive 
-    if (ratePlanIndex == 4)
-        cost *= 1.13;
+    cost += Number(document.getElementById('additionalCostInput').value);
+
+    // All plans except Essentials are tax inclusive
+    if (ratePlanIndex == 5) {
+        cost *= 1.0825;
+    }
 
     // do this after because taxes paid up front. 
     cost += Number(document.getElementById('deviceCostInput').value);
